f0c97077e7056faa1fcb63e0740bb35ff9045233
srinivas kasiriveni <kasiriveni@gmail.com>
2018-03-31 12:22:20 +0530

    adding the eslint support

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-import React,{Component} from 'react';
-import {render} from 'react-dom';
-import Register from '../component/form';
-import '../css/index.scss'
+/* eslint no-undef: "error" */
 
-const root = document.getElementById("root");
+import React from 'react';
+import { render } from 'react-dom';
+import Register from '../component/form';
+import '../css/index.scss';
+
+const root = document.getElementById('root');
 
 render(
-    <Register/>,
-    root
-)
-
-
+  <Register />,
+  root,
+);
 

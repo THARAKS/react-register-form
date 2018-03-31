94217f2a5165b8f3bac00dd6c7b05c114ec7b75e
srinivas kasiriveni <kasiriveni@gmail.com>
2018-03-31 12:44:40 +0530

    adding prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "globals": {
+        "document": false
+    },
     "extends": ["airbnb", "plugin:prettier/recommended"],
     "rules":{
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
